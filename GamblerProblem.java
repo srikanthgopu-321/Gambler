05986a74df3be9d55e89329f0c4aa760c1758835
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-11-23 17:57:50 +0530

    added:uc-2MakeBet

--- GamblerProblem.java
+++ GamblerProblem.java
@@ -3,5 +3,11 @@ public class GamblerProblem {
     public static final int BET = 1;
 
     public static void main(String[] args) {
+	checkGame();
+
       }
+	public static int checkGame(){
+        Random rand = new Random();
+        int result= rand.nextInt(2);
+        return result;
 }
