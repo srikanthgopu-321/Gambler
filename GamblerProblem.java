cef3f4106299a40d9f3bf7688029eece7f59ff0f
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-11-23 18:04:20 +0530

    added:50%of game win or loss

--- GamblerProblem.java
+++ GamblerProblem.java
@@ -10,4 +10,32 @@ public class GamblerProblem {
         Random rand = new Random();
         int result= rand.nextInt(2);
         return result;
+	}
+
+
+
+	public static void stakeCalculation(){
+        int daily_stake = STAKE;
+        int daily_bet = BET;
+
+        while(daily_stake >= 50 && daily_stake <= 150){
+            int outcome = checkGame();
+            if(outcome == 1 ){
+                daily_stake+=daily_bet;
+
+            }
+            else
+                daily_stake-=daily_bet;
+
+            if(daily_stake == 50){
+                System.out.println("loose "+daily_stake);
+                break;
+            }
+            if(daily_stake == 150 ){
+                System.out.println("Win "+daily_stake);
+                break;
+            }
+
+        }
+    }
 }
